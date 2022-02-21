8d4f5f3c91568b1961a17c653a40a829fd91924a
Fabian Reinders <fabian@fabiancdng.com>
2022-02-21 10:05:21 +0100

    🎉 CMS: Init

---
+++ cms/config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
+    },
+  },
+});
