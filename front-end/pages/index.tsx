0db5354cd0e64552333c724a0b507b34202fdfd0
Fabian Reinders <fabian@fabiancdng.com>
2022-02-21 13:58:24 +0100

    ✨ Front End: Add Homepage (use content from CMS)

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,7 +1,60 @@
 import type { NextPage } from 'next';
+import { env } from 'process';
+import { useEffect, useState } from 'react';
 
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world</h1>;
+/**
+ * Data (& content) for the homepage from CMS.
+ */
+interface homePageData {
+  title: string,
+  content: string,
+  updatedAt: string,
+  createdAt: string,
+  publishedAt: string,
+}
+
+const Home: NextPage<homePageData> = (data: homePageData) => {
+  useEffect(() => {
+    console.log(data);
+  }, []);
+
+  return (
+    <>
+      <div className="flex justify-center w-full mb-5">
+        <div className="w-full p-10 md:p-0 md:w-2/3">
+          <h1 className="text-4xl font-bold my-5">{ data['title'] }</h1>
+          <p>{ data['content'] }</p>
+        </div>
+      </div>
+    </>
+  );
 };
+
+export async function getServerSideProps() {
+  const { CMS_URL, CMS_ACCESS_TOKEN } = env;
+
+  // Retrieve homepage data (& content) from CMS on the server side.
+  const homepageDataRequest = await fetch(CMS_URL + '/api/homepage', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + CMS_ACCESS_TOKEN,
+    }
+  });
+
+  // Parse JSON response and get relevant data.
+  const homepageDataRaw = await homepageDataRequest.json();
+  const data: homePageData = {
+    title: homepageDataRaw['data']['attributes']['title'],
+    content: homepageDataRaw['data']['attributes']['content'],
+    updatedAt: homepageDataRaw['data']['attributes']['updatedAt'],
+    createdAt: homepageDataRaw['data']['attributes']['createdAt'],
+    publishedAt: homepageDataRaw['data']['attributes']['publishedAt'],
+  }
+
+  // Pass data/content from the CMS to the component as a prop.
+  return {
+    props: data,
+  }
+}
 
 export default Home;
