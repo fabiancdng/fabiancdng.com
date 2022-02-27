9a25cc11ba1ced4680d4f5b152d2018b0e49a7fe
Fabian Reinders <fabian@fabiancdng.com>
2022-02-27 16:41:29 +0100

    🐛 CMS: Fix: `utf8mb4` Charset Support

--- cms/config/database.js
+++ cms/config/database.js
@@ -8,6 +8,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL'),
+      charset: 'utf8mb4',
     },
   },
 });
