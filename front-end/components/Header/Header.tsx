7699f5772a7bcc2fc3e57ab38680d30ecccfa23d
Fabian Reinders <fabian@fabiancdng.com>
2022-05-08 17:31:09 +0200

    💄 Front End: Fix: Darkmode Issues With Mobile Nav

--- front-end/components/Header/Header.tsx
+++ front-end/components/Header/Header.tsx
@@ -79,8 +79,8 @@ const Header = ({ data, statics }: headerProps) => {
                     <DarkModeToggle />
                     <button
                         onClick={ () => setMobileMenuOpen(!mobileMenuOpen) }
-                        className={ `rounded p-2 border-white border transition-all
-                        ease-in-out duration-200 hover:bg-slate-200 bg-slate-100` }
+                        className={ `rounded p-2 transition-all
+                        ease-in-out duration-200 hover:bg-slate-200 bg-slate-100 dark:bg-slate-600 dark:hover:bg-slate-500 dark:text-white` }
                     >
                         <FaBars />
                     </button>
@@ -88,13 +88,20 @@ const Header = ({ data, statics }: headerProps) => {
             </nav>
 
             {/* Mobile menu */}
-            { mobileMenuOpen && <ul className="p-4 mt-2 block space-y-3 md:hidden bg-slate-100">
+            { mobileMenuOpen && <ul className="p-4 mt-2 block space-y-3 md:hidden bg-slate-100 dark:bg-slate-700">
 
                 {
                     // Render all header links (separate `HeaderLink` component).
                     data.links.data?.map((link, index) => (
                         // Single Navigation Link
-                        <li onClick={ () => setMobileMenuOpen(false) } key={ index } className="rounded bg-slate-200 hover:bg-slate-300 transition ease-in-out duration-300"><HeaderLink data={ link } additionalCSS="block w-full px-3 py-2" /></li>
+                        <li
+                            key={ index }
+                            onClick={ () => setMobileMenuOpen(false) }
+                            className="rounded bg-slate-200 hover:bg-slate-300 transition
+                            ease-in-out duration-300 dark:bg-slate-600 dark:hover:bg-slate-500"
+                        >
+                            <HeaderLink data={ link } additionalCSS="block w-full px-3 py-2" />
+                        </li>
                     ))
                 }
             </ul> }
