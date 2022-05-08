953f090425371e41f0f57301761eab56841af905
Fabian Reinders <fabian@fabiancdng.com>
2022-05-08 17:07:02 +0200

    ✨ Front End: Add Loading Indicator/Progress Bar
    Resolve #7

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import '../styles/global.css';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    // <GlobalContextProvider>
+    // <GlobalContextProvider></GlobalContextProvider>
     <div className="app overflow-x-hidden">
+      {/* Progress bar when (re)loading page */}
+      <NextNProgress
+        options={{
+          template: '<div class="bar" role="bar"><div class="peg"></div></div></div>'
+        }}
+      />
       <Component {...pageProps} />
     </div>
     // </GlobalContextProvider>
