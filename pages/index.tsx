9b2b9e6fbb76943132fc321a9070251a6d7f3f6a
Fabian Reinders <fabian@fabiancdng.com>
2022-10-21 14:34:30 +0200

    ✨ Front-End: Basic Page Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Homepage | fabiancdng.com</title>
+        <meta
+          name="description"
+          content={`Hey 👋 I'm Fabian, a student and full-stack web-developer. On this website, you can find some of my current projects and my blog posts about development and tech.`}
+        />
+      </Head>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 };
 
 export default Home;
