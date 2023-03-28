f7b3d0609eebed608766f4046a410ad4f894f6ff
Fabian Reinders <fabian@fabiancdng.com>
2023-03-28 22:44:28 +0200

    🎉 Get Basic Storyblok Rendering Working

---
+++ components/Storyblok/Page.tsx
@@ -0,0 +1,11 @@
+import { storyblokEditable, StoryblokComponent } from '@storyblok/react';
+
+const Page = ({ blok }: any) => (
+  <main className="text-center mt-4" {...storyblokEditable(blok)}>
+    {blok.body.map((nestedBlok) => (
+      <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+    ))}
+  </main>
+);
+
+export default Page;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import type { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
+import { storyblokInit, apiPlugin } from '@storyblok/react';
 import 'highlight.js/styles/atom-one-dark.css';
 import '../styles/ghost.css';
 import '../styles/global.css';
+import Page from '../components/Storyblok/Page';
+import Teaser from '../components/Storyblok/Teaser';
+import Grid from '../components/Storyblok/Grid';
+import Feature from '../components/Storyblok/Feature';
+
+// Map Storyblok components to Next.js components.
+const storyblokComponents = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: process.env['STORYBLOK_TOKEN'],
+  use: [apiPlugin],
+  components: storyblokComponents,
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
