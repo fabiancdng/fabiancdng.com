5331925a8b47f359bec5d32731862b619d14dc4d
Fabian Reinders <fabian@fabiancdng.com>
2023-05-21 13:24:24 +0200

    ✨ Add Utility Function to Get Images

--- src/adapters/ImageAdapter.ts
+++ src/adapters/ImageAdapter.ts
@@ -1,7 +1,23 @@
 import sizeOf from 'image-size';
 
-export function getImageDimensions(path: string) {
-  const absPath = process.cwd();
-  const dimensions = sizeOf(`${absPath}${path}`);
+export function getImageDimensions(absImagePath: string) {
+  const dimensions = sizeOf(absImagePath);
   return dimensions;
+}
+
+/**
+ * Returns absolute `fs` path of an image from the content directory.
+ */
+export function getImagePath(slug: string, filename: string) {
+  const absPath = process.cwd();
+  const path = `${absPath}/content${slug}/img/${filename}`;
+  return path;
+}
+
+/**
+ * Returns the URL for the `src` prop of an image from the content directory.
+ */
+export function getImageSource(slug: string, filename: string) {
+  const source = `/api/content/images${slug}/img/${filename}?token=${process.env.NEXT_PUBLIC_CONTENT_IMAGE_API_KEY}`;
+  return source;
 }
