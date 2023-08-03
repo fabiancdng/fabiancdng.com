41350fe7e80c9ac98a4a4952a8402efa060309f0
Fabian Reinders <fabian@fabiancdng.com>
2023-08-03 13:21:26 +0200

    ✨ Add WP Pages Support

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,8 +1,9 @@
-import { getAllPageSlugs, getPageBySlug } from '@/adapters/ContentAdapter';
 import { Metadata } from 'next';
 import { openGraphBaseMetadata, twitterBaseMetadata } from '@/app/metadata';
 import Page from '@/components/Page/Page';
 import { notFound } from 'next/navigation';
+import { env } from 'process';
+import { WP_Post } from '@/types';
 
 /**
  * If a request comes in to a page that exists in the file system, but has not been built yet,
@@ -19,27 +20,32 @@ export const revalidate = 30 * 60;
  * Dynamically/statically generate metadata for the page.
  */
 export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata | null> {
-  const page = await getPageBySlug(params.slug);
+  const wordPressEndpoint = `${env.WP_REST_API_URL}/wp/v2/pages?slug=${params.slug}&_embed`;
+  const postRequest = await fetch(wordPressEndpoint);
+  const pages: WP_Post[] = await postRequest.json();
+  const page = pages[0];
 
   if (!page) return null;
 
+  const robots = ['privacy-policy', 'legal-notice'];
+
   return {
-    title: `${page.metadata.title} | fabiancdng.com`,
-    description: page.metadata.description,
-    robots: page.metadata.search_engine_index === false ? 'noindex, follow' : undefined,
+    title: `${page.title.rendered} | fabiancdng.com`,
+    description: page.excerpt.rendered,
+    robots: robots.includes(page.slug) ? 'noindex, follow' : undefined,
     alternates: {
       canonical: `/${page.slug}`,
     },
     twitter: {
       ...twitterBaseMetadata,
-      title: `${page.metadata.title} | fabiancdng.com`,
-      description: page.metadata.description,
+      title: `${page.title.rendered} | fabiancdng.com`,
+      description: page.excerpt.rendered,
     },
     openGraph: {
       ...openGraphBaseMetadata,
       url: `/${page.slug}`,
-      title: `${page.metadata.title} | fabiancdng.com`,
-      description: page.metadata.description,
+      title: `${page.title.rendered} | fabiancdng.com`,
+      description: page.excerpt.rendered,
     },
   };
 }
@@ -48,7 +54,10 @@ export async function generateMetadata({ params }: { params: { slug: string } })
  * A page dynamically/statically generated from Markdown.
  */
 const DynamicPage = async ({ params }: { params: { slug: string } }) => {
-  const page = await getPageBySlug(params.slug);
+  const wordPressEndpoint = `${env.WP_REST_API_URL}/wp/v2/pages?slug=${params.slug}&_embed`;
+  const postRequest = await fetch(wordPressEndpoint);
+  const pages: WP_Post[] = await postRequest.json();
+  const page = pages[0];
 
   // If the page doesn't exist, return a 404.
   if (!page) {
@@ -66,8 +75,11 @@ const DynamicPage = async ({ params }: { params: { slug: string } }) => {
  * Export possible paths for this page.
  */
 export async function generateStaticParams() {
-  const pageSlugs = await getAllPageSlugs();
-  return pageSlugs.map((slug) => ({ slug }));
+  const wordPressEndpoint = env.WP_REST_API_URL + '/wp/v2/pages?_fields=slug';
+  const pageSlugsRequest = await fetch(wordPressEndpoint);
+  const pageSlugs = await pageSlugsRequest.json();
+
+  return pageSlugs.map((page: { slug: string }) => ({ slug: page.slug }));
 }
 
 export default DynamicPage;
--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,12 +1,10 @@
-import { Page } from '@/types';
-import Markdown from '../Markdown/Markdown';
+import { WP_Post } from '@/types';
 
-const Page = ({ page }: { page: Page }) => {
+const Page = ({ page }: { page: WP_Post }) => {
   return (
-    <article id="page" className="container pt-32 px-7 mx-auto mb-20 max-w-5xl text-black dark:text-white">
-      <div className="post-body max-w-3xl mx-auto">
-        <Markdown slug={page.slug} content={page.content} />
-      </div>
+    <article id="page" className="container pt-32 px-7 mx-auto mb-20 max-w-3xl text-black dark:text-white">
+      <h1 className="text-5xl font-bold">{page.title.rendered}</h1>
+      <div className="mt-10" dangerouslySetInnerHTML={{ __html: page.content.rendered }}></div>
     </article>
   );
 };
