7df87063d7666bdbccdbecee4c439ef8f13fdfbb
Fabian Reinders <fabian@fabiancdng.com>
2023-08-06 20:50:58 +0200

    ✨ New Revalidation REST API

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,9 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
 
-export async function GET(request: NextRequest) {
-  const token = request.nextUrl.searchParams.get('token') || '';
-  const path = request.nextUrl.searchParams.get('path') || '/';
+export async function POST(request: NextRequest) {
+  // Get token and path fields from the POST body.
+  const body = await request.json().catch(() => ({}));
+
+  if (body.token === undefined || body.path === undefined) {
+    return NextResponse.json({ success: false, message: 'Missing token or path.' }, { status: 400 });
+  }
+
+  const { token, path } = body;
 
   if (token !== process.env.ADMIN_API_KEY) {
     return NextResponse.json({ success: false, message: 'Unauthorized.' }, { status: 401 });
